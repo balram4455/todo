a5b98d56c5d1a40c2b0673551bfc0af1354cde6a
balram4455 <161879990+balram4455@users.noreply.github.com>
2024-05-04 19:17:13 +0530

    Add files via upload

---
+++ FirstwebappApplication.java
@@ -0,0 +1,13 @@
+package com.balram.firstwebapp.firstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstwebappApplication.class, args);
+	}
+
+}
